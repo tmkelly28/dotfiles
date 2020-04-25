8d19afdbbe91dee7fa0a395bb0da9cd28c1e1607
Thomas Kelly <tmkelly28@gmail.com>
2020-04-25 16:01:55 -0400

    create local vim override if not exists

--- init.sh
+++ init.sh
@@ -2,9 +2,13 @@
 
 set -e
 
+LOCAL_VIM_OVERRIDES=~/dotfiles/nvim/local.vim
+if [[ ! -f "$LOCAL_VIM_OVERRIDES" ]]; then
+  echo "Creating $LOCAL_VIM_OVERRIDES"
+  touch $LOCAL_VIM_OVERRIDES
+fi
+
 rm ~/.bash_profile && ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 rm ~/.gitignore_global && ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
 rm ~/.tmux.conf && ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-
-# rm ~/.config/nvim/init.vim && \
-  ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+rm ~/.config/nvim/init.vim && ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
