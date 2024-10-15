d1f63a6100dfb014cb564e16105bd9bd7804138a
Tom Kelly <tmkelly28@gmail.com>
2024-10-15 16:47:05 -0400

    add dash p

--- init.sh
+++ init.sh
@@ -43,8 +43,8 @@ CONFIG_FILES=(\
   "kitty/iceberg.conf" \
   "nvim/init.vim")
 
-mkdir ~/.config/kitty
-mkdir ~/.config/nvim
+mkdir -p ~/.config/kitty
+mkdir -p ~/.config/nvim
 for f in ${CONFIG_FILES[@]}; do
   TARGET="$HOME/.config/$f"
   SOURCE="$HOME/dotfiles/$f"
